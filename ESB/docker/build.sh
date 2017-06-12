9c156f4e6f3d9213a28cd7681419f3bd820240eb
Francesco Bruni <bruni@planetek.it>
2017-06-12 13:07:26 +0200

    Added no-cache flag when building the image

--- ESB/docker/build.sh
+++ ESB/docker/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t planetek/ubuntu-maven-git:base -f Dockerfile.base .
-docker build -t planetek/cmems_esb:$1 --build-arg branch=$1 .
+docker build --no-cache -t planetek/cmems_esb:$1 --build-arg branch=$1 .
