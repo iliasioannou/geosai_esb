5e6a1614e8ed65b67fe36eee18fee0e953bc70f9
Ilias Ioannou <ioannou@planetek.gr>
2017-11-21 16:08:55 +0100

    Updated build.sh

--- ESB/docker/build.sh
+++ ESB/docker/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t planetek/ubuntu-maven-git:base -f Dockerfile.base .
-docker build --no-cache -t planetek/cmems_esb:$1 --build-arg branch=$1 .
+docker build --no-cache -t planetek/eosai_esb:$1 --build-arg branch=$1 .
