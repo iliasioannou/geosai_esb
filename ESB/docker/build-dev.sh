48b3cfea2e2d6420211d67f9154a12e80de2508c
Ilias Ioannou <ioannou@planetek.gr>
2017-11-21 16:07:22 +0100

    Updated build-dev.sh

--- ESB/docker/build-dev.sh
+++ ESB/docker/build-dev.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t planetek/ubuntu-maven-git:base -f Dockerfile.base .
-docker build -t planetek/cmems_esb:dev -f Dockerfile.dev .
+docker build -t planetek/eosai_esb:dev -f Dockerfile.dev .
