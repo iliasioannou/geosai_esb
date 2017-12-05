ed2be937d6f9c6643a88ca8089143e8561435efb
Francesco Bruni <bruni@planetek.it>
2017-12-05 11:33:21 +0100

    Build final image using a multiimage approach

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-docker build -t planetek/ubuntu-maven-git:base -f Dockerfile.base .
-docker build --no-cache -t planetek/eosai_esb:$1 --build-arg branch=$1 .
+docker build -t dockerhub.planetek.it/ubuntu-maven-git-muleesb:base -f Dockerfile.base .
+docker push dockerhub.planetek.it/ubuntu-maven-git-muleesb:base
+
+docker build -t dockerhub.planetek.it/pkh111_eosai_esb:base -f Dockerfile.base.code .
+docker push dockerhub.planetek.it/pkh111_eosai_esb:base
+
+docker build --no-cache -t dockerhub.planetek.it/pkh111_eosai_esb:$1 --build-arg branch=$1 .
+docker push dockerhub.planetek.it/pkh111_eosai_esb:$1
